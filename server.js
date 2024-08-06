a861bc08f443b069e56b75416242cbf82274c80a
loussalmohammed <hplaptopsimo@gmail.com>
2024-08-06 15:18:00 +0100

    add signup functionality

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { Pool } = require('pg');
+const morgan = require('morgan');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 const port = 3000;
 
 // Set up EJS for templating
 app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -14,10 +22,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // PostgreSQL connection pool
 const pool = new Pool({
-  user: 'user',
-  host: 'host',
-  database: 'banque',
-  password: 'pass',
+  user: 'postgres',
+  host: '127.0.0.1',
+  database: 'BanqueSystem',
+  password: 'superman',
   port: 5432,
 });
 
@@ -34,9 +42,60 @@ app.listen(port, () => {
 app.get('/users', async (req, res) => {
     try {
       const result = await pool.query('SELECT * FROM users');
-      res.send(users = result.rows );
+      const users = result.rows;
+      res.render('users', {users: users, title: 'Page Users'});
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
     }
   });
+
+  
+app.get('/signUp', (req, res) => {
+    res.render('signup', {title: 'signup', css: './css/auth.css'});
+});
+
+app.post('/signupAction', async (req, res) => {
+        const { username, email, phone, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+        await pool.query(
+            'INSERT INTO users (username, email, phone, password) VALUES ($1, $2, $3, $4)',
+            [username, email, phone, hashedPassword]
+        );
+        res.redirect('/roles');
+    } catch(err) {
+        console.error(err.message);
+    }
+
+});
+
+app.get('/roles', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM roles');
+        const roles = result.rows;
+        res.render('roles', {roles: roles, title: 'Page Roles'});
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+      }
+    
+});
+
+app.get('/roleEdit', (req, res) => {
+    res.render('hello');
+});
+
+app.post('/addRole', async (req, res) => {
+    let role = req.body.role;
+    try {
+        await pool.query(`INSERT INTO ROLES (name) VALUES ($1)`, [role]);
+        res.redirect('/roles');
+    } catch(err) {
+        console.log(err.message);
+    }
+});
+
+app.use((req, res) => {
+    res.status(404).render('404', {title: 404})
+});
